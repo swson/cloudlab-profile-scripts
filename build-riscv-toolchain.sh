6d202928caab72613de456370ae3edd52c3c1b70
swson3 <swson3@node0.test.adsp-pg0.utah.cloudlab.us>
2026-03-15 13:46:02 -0600

    update build-riscv-toolchain.sh

--- build-riscv-toolchain.sh
+++ build-riscv-toolchain.sh
@@ -6,18 +6,18 @@
 
 echo "updating system and installing prerequisites..."
 sudo apt update
-sudo apt install autoconf automake autotools-dev curl python3 libmpc-dev libmpfr-dev libgmp-dev gawk build-essential bison flex texinfo gperf libtool patchutils bc zlib1g-dev libexpat-dev -y​
+sudo apt install autoconf automake autotools-dev curl python3 libmpc-dev libmpfr-dev libgmp-dev gawk build-essential bison flex texinfo gperf libtool patchutils bc zlib1g-dev libexpat-dev -y
 
 echo "cloning toolchain repository..."
 mkdir RISCV
 cd RISCV
-git clone --recursive https://github.com/riscv/riscv-gnu-toolchain​
+git clone --recursive https://github.com/riscv/riscv-gnu-toolchain
 cd riscv-gnu-toolchain
 
 echo "configuring and building for rv32gc..."
 mkdir build
 cd build
-../configure --prefix=/opt/riscv --with-arch=rv32gc –with-abi=ilp32d​
+../configure --prefix=/opt/riscv --with-arch=rv32gc --with-abi=ilp32d
 sudo make -j$(nproc) linux
 
 echo "adding the RISC-V toolchain to PATH"
