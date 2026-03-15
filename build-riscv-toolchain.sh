e3eefa3d7a256d92d016c38d378657c17f0e8beb
Seung Woo Son <10246534+swson@users.noreply.github.com>
2026-03-14 23:41:59 -0400

    Create build-riscv-toolchain.sh

---
+++ build-riscv-toolchain.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# referred online resources:
+#    https://github.com/riscv-collab/riscv-gnu-toolchain
+#    https://riscv.epcc.ed.ac.uk/documentation/how-to/install-toolchain/
+
+echo "updating system and installing prerequisites..."
+sudo apt update
+sudo apt install autoconf automake autotools-dev curl python3 libmpc-dev libmpfr-dev libgmp-dev gawk build-essential bison flex texinfo gperf libtool patchutils bc zlib1g-dev libexpat-dev -y​
+
+echo "cloning toolchain repository..."
+mkdir RISCV
+cd RISCV
+git clone --recursive https://github.com/riscv/riscv-gnu-toolchain​
+cd riscv-gnu-toolchain
+
+echo "configuring and building for rv32gc..."
+mkdir build
+cd build
+../configure --prefix=/opt/riscv --with-arch=rv32gc –with-abi=ilp32d​
+sudo make -j$(nproc) linux
+
+echo "add the RISC-V toolchain to PATH"
